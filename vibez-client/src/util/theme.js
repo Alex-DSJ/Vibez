12b6a0b7394db3fd9b0be150a3e2a78e5fb9afee
Shijun Deng <dengshijun233@gmail.com>
2020-03-23 03:13:37 -0400

    try again

---
+++ vibez-client/src/util/theme.js
@@ -0,0 +1,43 @@
+export default {
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastTest: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastTest: '#fff'
+    }
+  },
+  typography: {
+    useNextVariants: true
+  },
+  form: {
+    textAlign: 'center'
+  },
+  image: {
+    margin: '20px auto 20px auto'
+  },
+  pageTitle: {
+    margin: '10px auto 10px auto'
+  },
+  testField: {
+    margin: '10px auto 10px auto'
+  },
+  button: {
+    marginTop: 20,
+    position: 'relative'
+  },
+  customError: {
+    color: 'red',
+    fortSize: '0.8rem',
+    marginTop: 10
+  },
+  progress: {
+    position: 'absolute'
+  }
+};
